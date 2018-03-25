a872046f8ffafbf9e9f42a4f2940eb657ef5dc93
ibunao <idingran@163.com>
2018-03-26 07:44:21 +0800

    begin

---
+++ common/config/db-local.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=zbshop',
+    'username' => 'root',
+    'password' => '159753',
+    'charset' => 'utf8',
+];
---
+++ common/config/main.php
@@ -0,0 +1,17 @@
+<?php
+$db = array_merge(
+    require __DIR__ . '/db-local.php'
+);
+return [
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'db' => $db,
+    ],
+];
